5e9de5a800f590742456e9b93d011d5811e6e71a
ControlYourPotatoes <alexanderpuga78@gmail.com>
2024-01-05 19:24:53 -0400

    i need to finish this, max power

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator(): #look no arguments
+async def async_generator():
     """Coroutine that takes no arguments"""
     for _ in range(10):
         await asyncio.sleep(1)
