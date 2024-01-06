3e7ef9ff6252f1ccba046afe77d44be2eb27b9f2
ControlYourPotatoes <alexanderpuga78@gmail.com>
2024-01-05 21:23:38 -0400

    test this upgrade

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""import async_generator and write a coroutine"""
+"""Import async_generator from the previous
+task and then write a coroutine called async_comprehension
+that takes no arguments.
+
+The coroutine will collect 10 random numbers using an async
+comprehensing over async_generator, then return the 10
+random numbers."""
+from typing import List
 import asyncio
-import random
-from typing import Generator
-from importlib import import_module
-
-module_name = '0-async_generator'
-module = import_module(module_name)
-async_generator = module.async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
-    """coroutine that takes no arguments"""
+async def async_comprehension() -> List[float]:
+    """Returns a list of 10 random numbers"""
     return [i async for i in async_generator()]
